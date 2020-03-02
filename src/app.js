d4d7b326299fbcaf583ee7fe7332ce76ed53ddc3
root <root@ip-172-31-95-49.ec2.internal>
2020-03-02 20:17:30 +0000

    more changes

--- src/app.js
+++ src/app.js
@@ -28,6 +28,19 @@ app.use(function(req, res, next) {
         next();
 });
 
+function parseCookies (request) {
+    var list = {},
+        rc = request.headers.cookie;
+
+    rc && rc.split(';').forEach(function( cookie ) {
+        var parts = cookie.split('=');
+        list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return list;
+}
+
+
 
 app.get('/user/:Username', (req, res) => {
    console.log("Getting user: " + req.params.Username);
