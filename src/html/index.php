763dd5dc7b6ba2e38813c12f2412248c59674798
bacoole <55105881+bacoole@users.noreply.github.com>
2020-03-10 13:38:14 -0400

    updated index

--- src/html/index.php
+++ src/html/index.php
@@ -17,27 +17,58 @@ if (!isset($_COOKIE['username']))
   <body>
     <nav>
       <input type="text" name="search" placeholder=" Search...">
-      <a href="../Login-out/login.html">Sign In</a>
-      <a href="profile.php">Account</a>
-      <a href="logout.html">Sign Out</a>
+      <a href="Catalog.html">Catalog</a>
+      <a href="DriverOrders.html">Orders</a>
+      <a href="SponsorInfo.html">Sponsor Information</a>
+      <a href="DriverProfile.html">Account</a>
     </nav>
 
 
-      <section id="title-container">
-        <h1 id="top">Welcome to the</h1>
-        <h1 id="bottom">Good Driver</h1>
-        <h1>Incentive</h1>
-        <h1>Program</h1>
-      </section>
+    <h2>Welcome </h2> <h2 id="first">!</h2><br>
 
-      <section class="main-content" id="message-container">
-        <p>Welcome to the Good Driver Incentive Program...</p>
-        <a class="button" href="../Login-out/signup.html">Get Started</a>
-      </section>
+    <section id="points-table">
+      <h2>Current Points</h2><br>
+      <table id="points"></table>
+    </section>
 
+    <section id="sponsor-list">
+      <h2>Active Sponsorships</h2><br>
+    </section>
 
 
 
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="getCookie.js"></script>
+    <script type="text/javascript"charset="utf-8">
+  let getURL = "http://3.83.252.232:3001/user/" + getCookie("username");
+  $.ajax({
+              type: "GET",
+              url: getURL,
+              success: function (data) {
+                data = data[0];
+                document.getElementById("username").innerHTML = data["username"];
+                document.getElementById("points").innerHTML = data["overTime"];
+              }
+          });
+
+
+    getURL = "http://3.83.252.232:3001/user/" + getCookie("username", document)
+    $.ajax({
+                type: "GET",
+                url: getURL,
+                success: function (data) {
+                  let resultString = "<tr><th>Sponsor</th><th>Points</th></tr>";
+                  for(let i=0; i<data.length; i++){
+                    let info  = data[i];
+				            resultString = resultString + "<tr><td>" + info["sponsor"] + "</td><td>" + info["points"] + "</td></tr>";
+		              }
+		               console.log(resultString);
+		               document.getElementById("points").innerHTML = resultString;
+                }
+            });
+      </script>
 
 
   </body>
